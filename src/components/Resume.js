f74f7df0f358c2a87c537ade86d57796f2010467
ninafauziashari <40526004+ninafauziashari@users.noreply.github.com>
2021-03-01 10:54:38 +0800

    01/03/2021

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,6 @@ import EducationalBackground from '../resume/EducationalBackground';
 import WorkExperience from '../resume/WorkExperience';
 import Interests from '../resume/Interests'
 import Biodata  from '../resume/Biodata';
-import References from '../resume/References'
 import resumeData from '../resumeData'
 import Fade from 'react-reveal/Fade'
 import FileSaver from 'file-saver';
@@ -31,7 +30,6 @@ const Resume = () =>{
                         <EducationalBackground resumeData={resumeData}/>  
                         <WorkExperience resumeData={resumeData}/> 
                         <Interests resumeData={resumeData}/> 
-                        <References/>
                         
                     </Fade>
                     <div className="download-cv">
